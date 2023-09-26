ef6cfac4a76c031f12eb8399dd209f8272f0225c
Mister1324 <ezikovsemen072@gmail.com>
2023-09-26 06:05:02 +0300

    delete file_2_1.cpp

--- file_2_2.cpp
+++ file_2_2.cpp
@@ -11,7 +11,7 @@ float min_(float a, float b) {
 
 int main() {
     float x1, y1, x2, y2, x3, y3, a, b, c, p, s;
-    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
     a = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
     b = sqrt(pow((x2 - x3), 2) + pow((y2 - y3), 2));
     c = sqrt(pow((x1 - x3), 2) + pow((y1 - y3), 2));
